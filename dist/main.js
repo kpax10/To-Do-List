e951906b78cc587aea9ecaa204c2b79ff96e96c8
Kyle <notxap55@yahoo.com>
2023-03-03 10:51:29 -0800

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+function component() {
+  const element = document.createElement('div')
+  element.textContent = 'Testing';
+  console.log('fsd');
+
+  return element
+}
+
+document.body.appendChild(component())
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBOztBQUVBLHNDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG8tZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcbmZ1bmN0aW9uIGNvbXBvbmVudCgpIHtcbiAgY29uc3QgZWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpXG4gIGVsZW1lbnQudGV4dENvbnRlbnQgPSAnVGVzdGluZyc7XG4gIGNvbnNvbGUubG9nKCdmc2QnKTtcblxuICByZXR1cm4gZWxlbWVudFxufVxuXG5kb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGNvbXBvbmVudCgpKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
