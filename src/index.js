5afac60284fe46ed63cfa2a2fd85e24720528ac8
Kyle <notxap55@yahoo.com>
2023-10-26 16:45:14 -0700

    reworking file flow

--- src/index.js
+++ src/index.js
@@ -1,73 +1,8 @@
-import { removeDOMTasks, renderDOMTasks, deleteToDoListener } from './tasks.js';
+import { removeDOMTasks, renderDOMTasks, deleteToDoListener } from './to-dos.js';
 // Check local storage
 
 // Render Side Bar
 
 // Render main to-do's section
 
-// Create a todo array
-const CreateToDoArray = () => {
-  const list = [];
-  const getList = () => list;
 
-  return { list, getList }
-}
-
-export const toDoList = CreateToDoArray()
-
-// Create a class to create to-dos
-class ToDo {
-  constructor(task, note, date, project, priority) {
-    this.task = task
-    this.note = note
-    this.date = date
-    this.project = project
-    this.priority = priority
-  }
-  addToDo() {
-    toDoList.list.push(this)
-  }
-  // deleteToDo(toDo) {
-  //   toDoList.list.splice(toDoList.list.indexOf(toDo), 1)
-  // }
-}
-
-const button = document.querySelector('.submit')
-button.addEventListener('click', (e) => {
-  e.preventDefault()
-})
-
-const modal = document.querySelector('.modal')
-
-const addNewToDo = document.querySelector('.add-to-do')
-addNewToDo.addEventListener('click', (e) => {
-  e.preventDefault()
-  modal.classList.remove('hidden')
-})
-
-const removeModal = document.querySelector('.cancel')
-const form = document.querySelector('.form')
-removeModal.addEventListener('click', (e) => {
-  form.reset()
-  modal.classList.add('hidden')
-})
-
-const submitToDo = document.querySelector('.submit')
-submitToDo.addEventListener('click', () => {
-  // add to the array
-  const task = document.querySelector('#task').value
-  const note = document.querySelector('#note').value
-  const date = document.querySelector('#date').value
-  const project = document.querySelector('#project').value
-  const priority = document.querySelector('#priority').value
-
-  const newToDo = new ToDo(task, note, date, project, priority)
-  newToDo.addToDo()
-
-  removeDOMTasks()
-  renderDOMTasks()
-  deleteToDoListener()
-
-  modal.classList.add('hidden')
-  form.reset()
-})
---
+++ src/to-dos.js
@@ -0,0 +1,125 @@
+// Create a todo array
+const CreateToDoArray = () => {
+  const list = [];
+
+  return { list }
+}
+
+export const toDoList = CreateToDoArray()
+
+// Create a class to create to-dos
+class ToDo {
+  constructor(task, note, date, project, priority) {
+    this.task = task
+    this.note = note
+    this.date = date
+    this.project = project
+    this.priority = priority
+  }
+  addToDo() {
+    toDoList.list.push(this)
+  }
+}
+
+const modal = document.querySelector('.modal')
+
+const addNewToDo = document.querySelector('.add-to-do')
+addNewToDo.addEventListener('click', (e) => {
+  e.preventDefault()
+  modal.classList.remove('hidden')
+})
+
+const removeModal = document.querySelector('.cancel')
+const form = document.querySelector('.form')
+removeModal.addEventListener('click', (e) => {
+  form.reset()
+  modal.classList.add('hidden')
+})
+
+const submitToDo = document.querySelector('.submit')
+submitToDo.addEventListener('click', (e) => {
+  e.preventDefault()
+  // add to the array
+  const task = document.querySelector('#task').value
+  const note = document.querySelector('#note').value
+  const date = document.querySelector('#date').value
+  const project = document.querySelector('#project').value
+  const priority = document.querySelector('#priority').value
+
+  const newToDo = new ToDo(task, note, date, project, priority)
+  newToDo.addToDo()
+
+  removeDOMTasks()
+  renderDOMTasks()
+  deleteToDoListener()
+
+  modal.classList.add('hidden')
+  form.reset()
+})
+
+export function removeDOMTasks() {
+  const tasks = document.querySelector('.tasks')
+  while (tasks.hasChildNodes()) {
+    tasks.removeChild(tasks.firstChild)
+  }
+}
+
+export function renderDOMTasks() {
+  const tasks = document.querySelector('.tasks')
+
+  // loop over array, adding each task to the page
+  toDoList.list.forEach(element => {
+    const task = document.createElement('div')
+    const taskTitle = document.createElement('h3')
+    const note = document.createElement('p')
+    const date = document.createElement('p')
+    const project = document.createElement('p')
+    const priority = document.createElement('p')
+    const editBtn = document.createElement('button')
+    const deleteBtn = document.createElement('button')
+
+    task.classList.add('to-do')
+    tasks.appendChild(task)
+    taskTitle.textContent = element.task
+    task.append(taskTitle)
+    note.textContent = element.note
+    task.append(note)
+    date.textContent = element.date
+    task.append(date)
+
+    !project.textContent === ''
+      ? project.textContent = `Project: ${element.project}`
+      : project.textContent = element.project
+    task.append(project)
+
+    priority.textContent = element.priority
+    task.append(priority)
+    editBtn.textContent = "Edit"
+    editBtn.classList.add('edit-to-do')
+    task.append(editBtn)
+    deleteBtn.textContent = 'Delete'
+    deleteBtn.classList.add('delete-to-do')
+    task.append(deleteBtn)
+  });
+}
+
+function editToDoListener() {
+  const editBtns = document.querySelectorAll('edit-to-do')
+  editBtns.addEventListener('click', () => {
+    console.log('edit');
+  })
+}
+
+function deleteToDoListener() {
+  const deleteBtns = document.querySelectorAll('.delete-to-do')
+  deleteBtns.forEach(element => {
+    element.addEventListener('click', () => {
+      const array = Array.from(deleteBtns)
+      const index = array.indexOf(element)
+      toDoList.list.splice(index, 1)
+      removeDOMTasks()
+      renderDOMTasks()
+      deleteToDoListener()
+    })
+  });
+}
